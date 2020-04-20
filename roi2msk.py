88490f0a852bee3accd16297ec6b44543c23f5dd
Jaewon-Seo <sjw06144@gmail.com>
2020-04-21 00:25:54 +0900

    add & edit

--- roi2msk.py
+++ roi2msk.py
@@ -8,14 +8,14 @@ import pydicom
 
 
 rfolpath= "../data/roi"
-afolname= os.listdir(rfolpath)
-afolname.sort()
+afolname= sorted(os.listdir(rfolpath))
+
 rfolname= [roiext for roiext in afolname if roiext != '.ipynb_checkpoints']
 
 dipath= '../data/dcm/*'
-diname= glob.glob(dipath)
-diname.sort()
+diname= sorted(glob.glob(dipath))
 
+# roi to mask
 for fl in range(len(rfolname)):
 
     rpath= "../data/roi/"+rfolname[fl]+"/*"
@@ -41,29 +41,76 @@ for fl in range(len(rfolname)):
             loc[lo]= (x[lo],y[lo])
 
         locset.append(loc)
-    for rc in range(len(diname)):
-        ds= pydicom.dcmread(diname[rc])
-        imwid= ds.Columns
-        imhei= ds.Rows
-
-        img= np.zeros((imwid,imhei), np.uint8)
 
         for fw in range(len(locset)):
 
             img1= cv2.fillPoly(img, [locset[fw]], (255,255,255))
 
-        mskp= "../data/msktest/"
-        mskn= "%s.png" %rfolname[fl]
-        cv2.imwrite(mskp+mskn, img1)
-        oimg= cv2.imread(mskp+mskn)
+        cv2.imwrite("../data/msk/"+rfolname[fl]+'.png', img1)
 
-        rwid= round(int((imwid/imhei)*512)
-        rimg= cv2.resize(oimg, dsize=(rwid,512), interpolation=cv2.INTER_AREA)
-        gray= cv2.cvtColor(rimg, cv2.COLOR_BGR2GRAY)
 
-        bimg= np.zeros((512,512))
-        bimg[0:512, 0:rwid]= gray
-        cv2.imwrite("../data/msktest512/"+mskn, bimg)
+# make original image, mask image to 512size
+pathmsk= "../data/msk/*.png"
+folmsk= sorted(glob.glob(pathmsk))
 
-#         plt.figure(figsize=(10,10))
-#         plt.imshow(img1, cmap='gray')
+ori_img = sorted(glob.glob("../jin_data/original/img/*"))
+
+for f5 in range(len(folmsk)):
+
+    mskn = "%s.png" %rfolname[f5]
+
+    mspath=folmsk[f5]
+    oimg = cv2.imread(mspath)
+    mspath= ori_img[f5]
+    dimg = cv2.imread(mspath)
+
+    height, width , channel = oimg.shape
+#     print(oimg)
+    imhei= height
+    imwid= width
+
+    rwid= round(int((imwid/imhei)*512))
+
+#     assert round(int((/imwid)*512)) < 512
+
+    rimg= cv2.resize(oimg, dsize=(rwid,512), interpolation=cv2.INTER_AREA)
+    gray = cv2.cvtColor(rimg, cv2.COLOR_BGR2GRAY)
+    bimg= np.zeros((512,512))
+    bimg[0:512, 0:rwid] = gray
+
+    dimg= cv2.resize(dimg, dsize=(rwid,512), interpolation=cv2.INTER_AREA)
+    dimg = cv2.cvtColor(dimg, cv2.COLOR_BGR2GRAY)
+    dgray= np.zeros((512,512))
+    dgray[0:512, 0:rwid] = dimg
+
+
+    cv2.imwrite("../data/msk512dum/"+mskn, bimg)
+    cv2.imwrite("../data/ori512dum/"+mskn, dgray)
+
+#divide data
+
+import shutil
+
+divo= sorted(glob.glob('../data/ori512dum/*'))
+divm= sorted(glob.glob('../data/msk512dum/*'))
+
+
+# print(len(divf))
+
+for tes in range(0,40):
+    otest= divo[tes]
+    shutil.copy(otest, '../data/ori/test' )
+    mtest= divm[tes]
+    shutil.copy(mtest, '../data/msk/test' )
+
+for tra in range(40,200):
+    otra= divo[tra]
+    shutil.copy(otra, '../data/ori/train' )
+    mtra= divm[tra]
+    shutil.copy(mtra, '../data/msk/train' )
+
+
+# print(len(glob.glob("../data/img200/ori/test/*")))
+# print(len(glob.glob("../data/img200/msk/test/*")))
+# print(len(glob.glob("../data/img200/ori/train/*")))
+# print(len(glob.glob("../data/img200/msk/train/*")))
